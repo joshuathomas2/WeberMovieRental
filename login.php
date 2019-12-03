dde48f87aa260a0ae3f55e334b92b44659d79cb4
Josh Thomas <joshuathomas2@mail.weber.edu>
2019-12-02 22:46:21 -0700

    implemented index page and login page

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php
+
+echo '<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<title></title>
+		<meta charset="UTF-8">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="Josh Thomas">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+
+	<body>
+	    <div class="jumbotron jumbotron-fluid">
+	        <h1 class="display-4">Weber Movie Rental</h1>
+	        <p class="lead">Movie rental system for Weber State University students and faculty</p>
+        </div>
+        
+        
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title bg-warning">Error logging in (Login system has not been developed yet!)</h2>
+            </div>
+        </div>
+	</body>
+</html>';
